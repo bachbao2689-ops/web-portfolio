a599ad900781effd90c045c437349513bef050f4
BART <bou@MacBook-Bro.local>
2026-09-25 16:29:03 +0700

    fix: correctly prepend basePath for unoptimized ProjectImage tags

--- src/components/ProjectImage.tsx
+++ src/components/ProjectImage.tsx
@@ -2,5 +2,8 @@ import Image from 'next/image';
 import type { Artwork } from '@/data/projects';
 
 export default function ProjectImage({ image, priority = false, sizes = '(max-width: 700px) 90vw, 80vw', className = '' }: { image: Artwork; priority?: boolean; sizes?: string; className?: string }) {
-  return <Image className={`project-image ${className}`} src={image.src} alt={image.alt} width={image.width} height={image.height} sizes={sizes} priority={priority} />;
+  const isProd = process.env.NODE_ENV === 'production';
+  const src = isProd ? `/web-portfolio${image.src}` : image.src;
+  
+  return <Image className={`project-image ${className}`} src={src} alt={image.alt} width={image.width} height={image.height} sizes={sizes} priority={priority} />;
 }
